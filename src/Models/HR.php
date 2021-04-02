797bcf1cd44d5f0f988a9ab369929aed330b45c5
thanhtamtotaa <thanhtamtqno1@gmail.com>
2021-04-02 10:15:59 +0700

    Create HR.php

---
+++ src/Models/HR.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Thotam\ThotamHr\Models;
+
+use App\User;
+use Wildside\Userstamps\Userstamps;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class HR extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+    use Userstamps;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'key', 'hoten', 'ten', 'ngaysinh', 'ngaythuviec', 'active',
+    ];
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'hrs';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'key';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The "type" of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Get all of the users for the HR
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class, 'hr_key', 'key');
+    }
+}
