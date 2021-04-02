babb2ba598302a03838756335f015c8e5a1cf2c5
thanhtamtotaa <thanhtamtqno1@gmail.com>
2021-04-02 16:25:05 +0700

    Create CheckAccount.php

---
+++ src/Http/Middleware/CheckAccount.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Thotam\ThotamHr\Http\Middleware;
+
+use Auth;
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckAccount
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!!!Auth::user()->hr) {
+            return response()->view('auth.update-info',[
+                'title' => 'Đã có lỗi sảy ra...',
+                'error_code' => '403',
+                'error_description' => 'Không có quyền truy cập',
+                'text_xlarge' => 'Tài khoản chưa được liên kết với thông tin BFO<br>Vui lòng cung cấp thông tin để được trợ giúp',
+                ]);
+        } elseif (optional(Auth::user()->hr)->active===0) {
+            return response()->view('errors.dynamic',[
+                'title' => 'Tài khoản đã bị vô hiệu hóa',
+                'error_code' => '403',
+                'error_description' => 'Không có quyền truy cập',
+                'text_xlarge' => 'Vui lòng liên hệ phòng truyền thông để được trợ giúp',
+                ]);
+        } elseif (!!!optional(Auth::user()->hr)->active) {
+            return response()->view('errors.dynamic',[
+                'title' => 'Tài khoản chưa được kích hoạt',
+                'error_code' => '403',
+                'error_description' => 'Không có quyền truy cập',
+                'text_xlarge' => 'Vui lòng liên hệ phòng truyền thông để được trợ giúp',
+            ]);
+        } else {
+            return $next($request);
+        }
+    }
+}
