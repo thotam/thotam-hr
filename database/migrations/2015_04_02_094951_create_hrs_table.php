8a7392a07a615dca0aa1e8f85211826c07dec47c
thanhtamtotaa <thanhtamtqno1@gmail.com>
2021-04-02 10:16:03 +0700

    Create 2015_04_02_094951_create_hrs_table.php

---
+++ database/migrations/2015_04_02_094951_create_hrs_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHrsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hrs', function (Blueprint $table) {
+            $table->string('key', 10);
+            $table->string('hoten', 255);
+            $table->string('ten', 100);
+            $table->timestamp('ngaysinh')->nullable()->default(null);
+            $table->timestamp('ngaythuviec')->nullable()->default(null);
+            $table->boolean('active')->nullable()->default(null);
+            $table->primary('key');
+            $table->unsignedBigInteger('created_by')->nullable()->default(null);
+            $table->unsignedBigInteger('updated_by')->nullable()->default(null);
+            $table->unsignedBigInteger('deleted_by')->nullable()->default(null);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hrs');
+    }
+}
