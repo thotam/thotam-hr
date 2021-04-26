02e51ba8627f1a4188dc24291b1c0d5bed9bc39b
thanhtamtotaa <thanhtamtqno1@gmail.com>
2021-04-26 15:34:27 +0700

    Update MailHR.php

--- src/Models/MailHR.php
+++ src/Models/MailHR.php
@@ -2,11 +2,12 @@
 
 namespace Thotam\ThotamHr\Models;
 
+use Thotam\ThotamHr\Models\HR;
 use Wildside\Userstamps\Userstamps;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class MailHR extends Model
 {
