69b5a9d93ca4ab7702da4f27d5ddacc5b45bc696
thanhtamtotaa <thanhtamtqno1@gmail.com>
2021-04-26 15:01:52 +0700

    Create MailHR.php

---
+++ src/Models/MailHR.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Thotam\ThotamHr\Models;
+
+use Wildside\Userstamps\Userstamps;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class MailHR extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+    use Userstamps;
+
+    /**
+     * Disable Laravel's mass assignment protection
+     *
+     * @var array
+     */
+    protected $guarded = ['id'];
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'mail_hrs';
+
+    /**
+     * Get the hr that owns the MailHR
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function hr(): BelongsTo
+    {
+        return $this->belongsTo(HR::class, 'hr_key', 'key');
+    }
+}
