5340b7c18b4d0239917a95da6dc67a3b7bde8532
thanhtamtotaa <thanhtamtqno1@gmail.com>
2021-05-29 08:36:20 +0700

    Update CheckHR.php

--- src/Http/Middleware/CheckHR.php
+++ src/Http/Middleware/CheckHR.php
@@ -17,7 +17,7 @@ class CheckHR
      */
     public function handle(Request $request, Closure $next)
     {
-        $hr = optional(optional(Auth::user())->hr);
+        $hr = optional(Auth::user())->hr;
         if (!!!$hr) {
             return response()->view('thotam-hr::auth.update-info',[
                 'title' => 'Đã có lỗi sảy ra...',
@@ -25,14 +25,14 @@ class CheckHR
                 'error_description' => 'Không có quyền truy cập',
                 'text_xlarge' => 'Tài khoản chưa được liên kết với thông tin nhân sự<br>Vui lòng cung cấp thông tin để được trợ giúp',
                 ]);
-        } elseif ($hr->active===0) {
+        } elseif (optional($hr)->active === 0) {
             return response()->view('errors.dynamic',[
                 'title' => 'Hồ sơ đã bị vô hiệu hóa',
                 'error_code' => '403',
                 'error_description' => 'Không có quyền truy cập',
                 'text_xlarge' => 'Vui lòng liên hệ phòng truyền thông để được trợ giúp',
                 ]);
-        } elseif (!!!$hr->active) {
+        } elseif (!!!optional($hr)->active) {
             return response()->view('errors.dynamic',[
                 'title' => 'Hồ sơ chưa được kích hoạt',
                 'error_code' => '403',
