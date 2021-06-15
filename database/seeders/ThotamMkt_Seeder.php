f1c4794996730d57d49c75dcad7d44aa7c2fb490
thanhtamtotaa <thanhtamtqno1@gmail.com>
2021-06-15 17:55:45 +0700

    Create ThotamMkt_Seeder.php

---
+++ database/seeders/ThotamMkt_Seeder.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace Thotam\ThotamHr\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Thotam\ThotamMkt\Models\MktSubTeam;
+
+class ThotamMkt_Seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //MktSubTeam
+        MktSubTeam::updateOrCreate(
+            ['id' => 1],
+            [
+                'name' => "Mentor",
+                'tag' => "Mentor",
+                'order' => 999,
+                'selectable' => false,
+                'active' => true
+            ]
+        );
+        MktSubTeam::updateOrCreate(
+            ['id' => 2],
+            [
+                'name' => "Sunshine",
+                'tag' => "S",
+                'order' => 1,
+                'selectable' => true,
+                'active' => true
+            ]
+        );
+        MktSubTeam::updateOrCreate(
+            ['id' => 3],
+            [
+                'name' => "Mountain floating",
+                'tag' => "MN",
+                'order' => 2,
+                'selectable' => true,
+                'active' => true
+            ]
+        );
+        MktSubTeam::updateOrCreate(
+            ['id' => 4],
+            [
+                'name' => "Mountain sank",
+                'tag' => "MC",
+                'order' => 3,
+                'selectable' => true,
+                'active' => true
+            ]
+        );
+        MktSubTeam::updateOrCreate(
+            ['id' => 5],
+            [
+                'name' => "Ocean",
+                'tag' => "O",
+                'order' => 4,
+                'selectable' => true,
+                'active' => true
+            ]
+        );
+
+        MktSubTeam::updateOrCreate(
+            ['id' => 6],
+            [
+                'name' => "Progermila",
+                'tag' => "Pro",
+                'order' => 100,
+                'selectable' => true,
+                'active' => true
+            ]
+        );
+        MktSubTeam::updateOrCreate(
+            ['id' => 7],
+            [
+                'name' => "Laforin",
+                'tag' => "Laf",
+                'order' => 100,
+                'selectable' => true,
+                'active' => true
+            ]
+        );
+        MktSubTeam::updateOrCreate(
+            ['id' => 8],
+            [
+                'name' => "Nebusal/Mucome",
+                'tag' => "Nemu",
+                'order' => 100,
+                'selectable' => true,
+                'active' => true
+            ]
+        );
+        MktSubTeam::updateOrCreate(
+            ['id' => 9],
+            [
+                'name' => "Aricamun",
+                'tag' => "Ari",
+                'order' => 100,
+                'selectable' => true,
+                'active' => true
+            ]
+        );
+
+        MktSubTeam::updateOrCreate(
+            ['id' => 10],
+            [
+                'name' => "Truyền thông nội bộ",
+                'tag' => "TTNB",
+                'order' => 100,
+                'selectable' => true,
+                'active' => true
+            ]
+        );
+        MktSubTeam::updateOrCreate(
+            ['id' => 11],
+            [
+                'name' => "Upharma",
+                'tag' => "U",
+                'order' => 100,
+                'selectable' => true,
+                'active' => true
+            ]
+        );
+        MktSubTeam::updateOrCreate(
+            ['id' => 12],
+            [
+                'name' => "Chiến dịch Poster",
+                'tag' => "Poster",
+                'order' => 100,
+                'selectable' => true,
+                'active' => true
+            ]
+        );
+        MktSubTeam::updateOrCreate(
+            ['id' => 13],
+            [
+                'name' => "MKT Đà Nẵng",
+                'tag' => "ĐN",
+                'order' => 100,
+                'selectable' => true,
+                'active' => true
+            ]
+        );
+        MktSubTeam::updateOrCreate(
+            ['id' => 14],
+            [
+                'name' => "MKT Hồ Chí Minh",
+                'tag' => "HCM",
+                'order' => 100,
+                'selectable' => true,
+                'active' => true
+            ]
+        );
+        MktSubTeam::updateOrCreate(
+            ['id' => 15],
+            [
+                'name' => "Check Image",
+                'tag' => "CI",
+                'order' => 100,
+                'selectable' => true,
+                'active' => true
+            ]
+        );
+        MktSubTeam::updateOrCreate(
+            ['id' => 16],
+            [
+                'name' => "Hỗ trợ Kỹ thuật",
+                'tag' => "KT",
+                'order' => 100,
+                'selectable' => true,
+                'active' => true
+            ]
+        );
+
+        MktSubTeam::updateOrCreate(
+            ['id' => 111],
+            [
+                'name' => "Team Fire",
+                'tag' => "Fire",
+                'order' => 888,
+                'selectable' => true,
+                'active' => true
+            ]
+        );
+
+    }
+}
