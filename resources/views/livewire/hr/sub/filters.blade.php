a000a2ef319d0a6c039fb32f68b83662a4c2c3b1
thotam <thanhtamtqno1@gmail.com>
2021-09-27 13:43:54 +0700

    Update filters.blade.php

--- resources/views/livewire/hr/sub/filters.blade.php
+++ resources/views/livewire/hr/sub/filters.blade.php
@@ -9,6 +9,12 @@
                 <button type="button" class="btn btn-success waves-effect" wire:click.prevent="add_hr" wire:loading.attr="disabled" thotam-blockui><span class="fas fa-plus-circle mr-2"></span>Thêm</button>
                 @endif
             </div>
+            <label class="form-label"></label>
+            <div class="col px-0 mb-1 text-md-left text-center">
+                @if ($hr->can("add-hr"))
+                <button type="button" class="btn btn-success waves-effect" wire:click.prevent="add_hr" wire:loading.attr="disabled" thotam-blockui><span class="fas fa-plus-circle mr-2"></span>Thêm</button>
+                @endif
+            </div>
         </div>
 
         <div class="col-md-auto mb-2">
