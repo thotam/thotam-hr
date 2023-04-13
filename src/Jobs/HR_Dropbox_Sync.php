ab9f58048c7a461d0c3669f20814533abf6f8d0e
thotam <thanhtamtqno1@gmail.com>
2023-04-13 09:56:17 +0700

    Update HR_Dropbox_Sync.php

--- src/Jobs/HR_Dropbox_Sync.php
+++ src/Jobs/HR_Dropbox_Sync.php
@@ -17,74 +17,74 @@ use Illuminate\Contracts\Queue\ShouldBeUnique;
 
 class HR_Dropbox_Sync implements ShouldQueue
 {
-	use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-	use JobTrait;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use JobTrait;
 
-	public $UserName, $Password;
+    public $UserName, $Password;
 
-	/**
-	 * Create a new job instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-	}
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    }
 
-	/**
-	 * Execute the job.
-	 *
-	 * @return void
-	 */
-	public function handle()
-	{
-		$this->UserName = config('thotam-upharma.API.User.UserName');
-		$this->Password = config('thotam-upharma.API.User.Password');
-		$__getAccount = $this->__getAccount();
-		$this->Token = $__getAccount['Token'];
-		$this->uPharmaID = $__getAccount['UserInfo']['uPharmaID'];
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->UserName = config('thotam-upharma.API.User.UserName');
+        $this->Password = config('thotam-upharma.API.User.Password');
+        $__getAccount = $this->__getAccount("UP000197");
+        $this->Token = $__getAccount['Token'];
+        $this->uPharmaID = $__getAccount['UserInfo']['uPharmaID'];
 
-		$mnvs =  HR::where('dropbox', true)->pluck('key')->toArray();
+        $mnvs =  HR::where('dropbox', true)->pluck('key')->toArray();
 
-		$response = Http::withHeaders([
-			'Content-Type' => 'application/json',
-		])->post(config('thotam-upharma.API.Employee.GetEmployeeInfoLst'), [
-			"uPharmaID" => $this->uPharmaID,
-			"Token" => $this->Token,
-		]);
+        $response = Http::withHeaders([
+            'Content-Type' => 'application/json',
+        ])->post(config('thotam-upharma.API.Employee.GetEmployeeInfoLst'), [
+            "uPharmaID" => $this->uPharmaID,
+            "Token" => $this->Token,
+        ]);
 
-		if ($response->status() == 200) {
-			$json_array = $response->json();
-			if ($json_array["RespCode"] == 0) {
-				$datas = collect($json_array["EmployeeLst"])->whereNotIn('uPharmaIDCode', $mnvs)->whereNotNull('uPharmaIDCode')->sortBy('uPharmaIDCode');
+        if ($response->status() == 200) {
+            $json_array = $response->json();
+            if ($json_array["RespCode"] == 0) {
+                $datas = collect($json_array["EmployeeLst"])->whereNotIn('uPharmaIDCode', $mnvs)->whereNotNull('uPharmaIDCode')->sortBy('uPharmaIDCode');
 
-				foreach ($datas as $data) {
-					if (!!$data["uPharmaIDCode"] && !!$data["EmployeeName"]) {
-						$import_hoten = mb_convert_case(trim($data["EmployeeName"]), MB_CASE_TITLE, "UTF-8");
-						$import_names = explode(' ', $import_hoten);
-						$import_ten = array_pop($import_names);
+                foreach ($datas as $data) {
+                    if (!!$data["uPharmaIDCode"] && !!$data["EmployeeName"]) {
+                        $import_hoten = mb_convert_case(trim($data["EmployeeName"]), MB_CASE_TITLE, "UTF-8");
+                        $import_names = explode(' ', $import_hoten);
+                        $import_ten = array_pop($import_names);
 
-						HR::updateOrCreate([
-							'key' => $data["uPharmaIDCode"],
-						], [
-							'hoten' => $import_hoten,
-							'ten' => $import_ten,
-							'ngaysinh' => !!$data['BirthDay'] ? (is_numeric($data['BirthDay']) ? \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($data['BirthDay'])->format('d-m-Y') : Carbon::parse(str_replace("/", "-", $data['BirthDay']))->format('d-m-Y')) : null,
-							'ngaythuviec' => !!$data['TranningTimeStart'] ? (is_numeric($data['TranningTimeStart']) ? \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($data['TranningTimeStart'])->format('d-m-Y') : Carbon::parse(str_replace("/", "-", $data['TranningTimeStart']))->format('d-m-Y')) : null,
-							'active' => true,
-							'dropbox' => true,
-							'sync' => false,
-							'phone' => !!collect($data)->get("Phone") ? collect($data)->get("Phone") : null,
-						]);
-					} else {
-						break;
-					}
-				}
-			} else {
-				throw new \Exception(get_class($this) . ': __fetchData - ' .  $json_array["RespCode"] . " " . $json_array["RespText"]);
-			}
-		} else {
-			throw new \Exception(get_class($this) . ': Unexpected HTTP status: __fetchData - ' .  $response->status() . ' - ' . $response->getReasonPhrase());
-		}
-	}
+                        HR::updateOrCreate([
+                            'key' => $data["uPharmaIDCode"],
+                        ], [
+                            'hoten' => $import_hoten,
+                            'ten' => $import_ten,
+                            'ngaysinh' => !!$data['BirthDay'] ? (is_numeric($data['BirthDay']) ? \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($data['BirthDay'])->format('d-m-Y') : Carbon::parse(str_replace("/", "-", $data['BirthDay']))->format('d-m-Y')) : null,
+                            'ngaythuviec' => !!$data['TranningTimeStart'] ? (is_numeric($data['TranningTimeStart']) ? \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($data['TranningTimeStart'])->format('d-m-Y') : Carbon::parse(str_replace("/", "-", $data['TranningTimeStart']))->format('d-m-Y')) : null,
+                            'active' => true,
+                            'dropbox' => true,
+                            'sync' => false,
+                            'phone' => !!collect($data)->get("Phone") ? collect($data)->get("Phone") : null,
+                        ]);
+                    } else {
+                        break;
+                    }
+                }
+            } else {
+                throw new \Exception(get_class($this) . ': __fetchData - ' .  $json_array["RespCode"] . " " . $json_array["RespText"]);
+            }
+        } else {
+            throw new \Exception(get_class($this) . ': Unexpected HTTP status: __fetchData - ' .  $response->status() . ' - ' . $response->getReasonPhrase());
+        }
+    }
 }
